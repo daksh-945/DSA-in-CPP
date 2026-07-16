ded15418c94df3adc3e444d2517022d441b9a641
daksh-945 <dakshlucs@Dakshs-MacBook-Air.local>
2026-07-17 04:08:49 +0530

    Add Pattern 2

--- Striver-A2Z/Patterns/Pattern_02.cpp
+++ Striver-A2Z/Patterns/Pattern_02.cpp
@@ -1,13 +1,13 @@
-// Problem: rectangle Pattern 
+// Problem: slope triangle pattern 
 // Source: Striver A2Z Sheet
-// link: https://takeuforward.org/plus/dsa/problems/pattern-1?subject=dsa 
+// link: https://takeuforward.org/plus/dsa/problems/pattern-2?subject=dsa
 // Difficulty: Easy
 
 class Solution {
 public:
-    void pattern1(int n) {
+    void pattern2(int n) {
         for (int i = 0 ; i < n; i++) {
-            for ( int j = 0 ; j < n; j++) {
+            for (int j =0 ; j <= i; j++) {
                 cout << "*";
             }
             cout << endl;
