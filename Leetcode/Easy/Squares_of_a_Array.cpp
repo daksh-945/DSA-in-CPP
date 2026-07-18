66eaee3edb461ce87b6a5ba59f6da74a2986cf9e
daksh-945 <dakshlucs@Dakshs-MacBook-Air.local>
2026-07-18 06:29:43 +0530

    Add Leetcode_977 problem

---
+++ Leetcode/Easy/Squares_of_a_Array.cpp
@@ -0,0 +1,26 @@
+// Problem_977 : Squares of a sorted array
+
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        // sorting abho ache se nhi atti nhi toh aur koi sorting kar taki time complexity bhi kam ho 
+        vector<int> vec;
+        for (int i= 0 ; i < nums.size(); i++) {
+            vec.push_back(nums[i]*nums[i]);
+        }
+        int n= vec.size();
+        for (int i=0 ; i < n; i++) {
+            for (int j=0 ;j < n-i-1 ; j++) {
+                if (vec[j] > vec[j+1] ) {
+                    int temp = vec[j];
+                    vec[j] = vec[j+1];
+                    vec[j+1] = temp;
+
+                }
+            }
+        }
+        return vec;
+        
+    }
+};
