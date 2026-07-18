d9012b00ddbfc807b87919984064ebc8e7e20bd8
daksh-945 <dakshlucs@Dakshs-MacBook-Air.local>
2026-07-18 06:13:03 +0530

    Add Leetcode problem_88

---
+++ Leetcode/Easy/Merge_sorted_array.cpp
@@ -0,0 +1,29 @@
+// Problem_88 : Merge Sorted Array 
+
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> vec;
+        // vec.insert(nums1.begin(), nums1.end());
+        // vec.insert
+        for (int i = 0 ; i < m ; i++) {
+            vec.push_back(nums1[i]);
+        }
+        for (int i =0 ; i < n ; i++) {
+            vec.push_back(nums2[i]);
+        }
+        for (int i= 0 ; i < n+m ; i++) {
+            for (int j = 0 ; j < n+m - i - 1 ; j++) {
+                if (vec[j] > vec[j+1]) {
+                    int temp = vec[j];
+                    vec[j] = vec[j+1];
+                    vec[j+1] = temp;
+                }
+            }
+        }
+        for (int i=0 ; i < n+m; i++){
+            nums1[i] = vec[i];
+        }
+    }
+};
