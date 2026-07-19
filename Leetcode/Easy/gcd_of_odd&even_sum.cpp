e001c7367b2dbf77f6fdfb69c031d3a35731129f
daksh-945 <daksh945saxena@gmail.com>
2026-07-19 06:47:39 +0530

    Add Leetcode_3658 Q solution

---
+++ Leetcode/Easy/gcd_of_odd&even_sum.cpp
@@ -0,0 +1,23 @@
+// Leetcode_3658 : gcd of odd and even sums 
+// beats : 100%;
+
+
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        int sodd = 0 , seven = 0 ;
+        // int c = 1;
+        int codd = 0 , ceven = 0;
+        for (int i = 1 ; ceven < n || codd < n; i++) {
+            if (i % 2 == 0) {
+                ceven++;
+                seven = seven + i;
+            }
+            else {
+                codd++;
+                sodd = sodd + i;
+            }
+        }
+        return gcd(seven,sodd);
+    }
+};
